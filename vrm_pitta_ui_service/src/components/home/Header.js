75efb5343801178db7163caebee1fb64b90fdc09
Prabhakar Singh <pksingh15007@gmail.com>
2021-08-04 22:15:34 +0530

    Resolved I.9
    I.9. Remove Featured Blog and Latest Blog and keep Featured service only in service hover component.

--- vrm_pitta_ui_service/src/components/home/Header.js
+++ vrm_pitta_ui_service/src/components/home/Header.js
@@ -20,113 +20,30 @@ const myHTML = `
               <nav class="wsmenu clearfix blue-header">
                 <ul class="wsmenu-list">
 
-                    <li aria-haspopup="true"><a href="/services">Services <span class="wsarrow"></span></a>
-	            						<div class="wsmegamenu clearfix">
-	             							<div class="container">
-	               								<div class="row">
+					<li aria-haspopup="true"><a href="/services">Services <span class="wsarrow"></span></a>
+						<div class="wsmegamenu clearfix halfmenu">
+							<div class="container-fluid">
+								<div class="row">
+									<!-- Links -->
+									<ul class="col-lg-6 col-md-12 col-xs-12 link-list">
+										<li><a href="/service1">Search Engine Optimisation</a></li>
+										<li><a href="/service2">Content Writing Services</a></li>
+										<li><a href="/service3">App Search Optimisation</a></li>
+										<li><a href="/service5">Creative Design Services</a></li>
+										<li><a href="/service4">Digital Strategy & Consulting</a></li>
+									</ul>
 
-
-	               									<!-- MEGAMENU QUICK LINKS -->
-													<div class="col-md-12 col-lg-3">
-
-														<!-- Title -->
-														<h3 class="title">Featured Services :</h3>
-
-														<ul class="link-list">
-														<li><a href="/service1">Search Engine Optimisation</a></li>
-														<li><a href="/service2">Content Writing Services</a></li>
-														<li><a href="/service3">App Search Optimisation</a></li>
-														<li><a href="/service4">Digital Strategy & Consulting</a></li>
-														<li><a href="/service5">Creative Design Services</a></li>
-														<li><a href="/service6">React.js Development</a></li>
-														<li><a href="/service7">Node.js Development</a></li>
-														<li><a href="/service8">Responsive Web Design</a></li>
-														<li><a href="/service9">Intuitive UI / UX designs</a></li>
-													   </ul>
-
-										            </div>	<!-- END MEGAMENU QUICK LINKS -->
-
-
-									                <!-- MEGAMENU FEATURED NEWS -->
-									                <div class="col-md-12 col-lg-5">
-
-									                	<!-- Title -->
-									                    <h3 class="title">Featured Blog:</h3>
-
-									                    <!-- Image -->
-									                    <div class="fluid-width-video-wrapper mb-15"><img src="./assets/images/blog/featured-news.jpg" alt="featured-news" /></div>
-
-									                    <!-- Text -->
-									                    <h5 class="h5-md">
-									                    	<a href="/single-post1">Things to know while performing SEO for multiple location
-										                         businesses
-										                    </a>
-									                    </h5>
-									                    <!--<p class="wsmwnutxt">Porta semper lacus cursus, feugiat primis ultrice in ligula risus auctor
-									                       tempus feugiat dolor impedit magna...
-									                    </p>-->
-
-									                </div>	<!-- END MEGAMENU FEATURED NEWS -->
-
-
-													<!-- MEGAMENU LATEST NEWS -->
-													<div class="col-md-12 col-lg-4">
-	
-														<!-- Title -->
-														<h3 class="title">Latest Blogs:</h3>
-	
-														<!-- Latest News -->
-														<ul class="latest-news">
-										
-															<!-- Post #1 -->
-															<li class="clearfix d-flex align-items-center">
-	
-																<!-- Image -->
-																<img class="img-fluid" src="./assets/images/blog/latest-post-1.jpg" alt="blog-post-preview" />
-	
-																<!-- Text -->
-																<div class="post-summary">
-																	<a href="/single-post2">We offer unique SEO services to your organization ...</a>
-																	<p>03 hours ago</p>
-																</div>
-	
-															</li>
-															
-															<!-- Post #2 -->
-															<li class="clearfix d-flex align-items-center">
-	
-																<!-- Image -->
-																<img class="img-fluid" src="./assets/images/blog/latest-post-2.jpg" alt="blog-post-preview" />
-	
-																<!-- Text -->
-																<div class="post-summary">
-																	<a href="/single-post3">Well-written, spoken content involves viewers ...
-																	</a>
-																	<p>Feb 02, 2020</p>
-																</div>
-	
-															</li>
-															
-															<!-- Post #3 -->
-															<li class="clearfix d-flex align-items-center">
-	
-																<!-- Image -->
-																<img class="img-fluid" src="./assets/images/blog/latest-post-3.jpg" alt="blog-post-preview" />
-	
-																<!-- Text -->
-																<div class="post-summary">
-																	<a href="/single-post4">Increase your user discovery; Tell your audience that you are here! ...</a>
-																	<p>Jan 29, 2020</p>
-																</div>
-	
-															</li>
-														</ul>
-													</div>	<!-- END MEGAMENU LATEST NEWS -->
-
-								                </div>  <!-- End row -->
-								            </div>  <!-- End container -->
-								        </div>  <!-- End wsmegamenu -->
-								    </li>
+									<!-- Links -->
+									<ul class="col-lg-6 col-md-12 col-xs-12 link-list">
+										<li><a href="/service6">React.js Development</a></li>
+										<li><a href="/service7">Node.js Development</a></li>
+										<li><a href="/service8">Responsive Web Design</a></li>
+										<li><a href="/service9">Intuitive UI / UX designs</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</li>	<!-- END DROPDOWN MENU -->
 
                     <li class="nl-simple" aria-haspopup="true"><a href="/pricing">Pricing</a></li>
 
