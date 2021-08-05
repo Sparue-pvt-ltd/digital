c3b25be2baff5f23093c48f63abefe8606b8be7e
Prabhakar Singh <pksingh15007@gmail.com>
2021-08-05 13:37:32 +0530

    Update the Footer
    Format all.css file
    umcomment some css and js file in index.html

--- vrm_pitta_ui_service/src/layouts/DefaultLayout.js
+++ vrm_pitta_ui_service/src/layouts/DefaultLayout.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "../components/home/Header";
-import Footer from "../components/common/Footer";
 
 const defaultLayout = ({ children }) => {
   return (
     <>
       <Header/>
       <main>{children}</main>
-      <Footer/>
     </>
   );
 };
